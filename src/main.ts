e506dd2e92c4ff0d3218806c4c65f152289cd8a2
Sesuraj-git <71758363+Sesuraj-git@users.noreply.github.com>
2025-12-02 11:35:57 +0530

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.setGlobalPrefix('api'); // optional
+  app.setGlobalPrefix('api/os'); // optional
 
   const config = new DocumentBuilder()
     .setTitle('Orders Service')
